ee3f1691cf64b3562ef5cb4d4dc52c8144d684e5
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-01-07 10:38:04 +0100

    EoD
    10:37 07/01/2026

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 
+// select dropdown element.
 const select = document.getElementById("projectSelect");
 
+// when selection canges-
 select.addEventListener("change", function () {
+  // -select target-
   const targetId = this.value;
+  // -if there is no target, stop.
   if (!targetId) return;
 
+  // smooth scroll to target.
   document.getElementById(targetId).scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+const backToTopButton = document.getElementById("backToTop");
+
+// Show button after scrolling down
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add("show");
+  } else {
+      backToTopButton.classList.remove("show");
+  }
+});
+
+// Scroll back to top when clicked
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
     behavior: "smooth"
   });
 });
