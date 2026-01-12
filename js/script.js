f0ffcaca9e7bc52ae0fe76f4bdc389ff144c2a18
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-01-12 14:45:11 +0100

    EoD

--- js/script.js
+++ js/script.js
@@ -14,22 +14,3 @@ select.addEventListener("change", function () {
     behavior: "smooth"
   });
 });
-
-const backToTopButton = document.getElementById("backToTop");
-
-// Show button after scrolling down
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    backToTopButton.classList.add("show");
-  } else {
-      backToTopButton.classList.remove("show");
-  }
-});
-
-// Scroll back to top when clicked
-backToTopButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
